b5c7ecab4a3d8dd18280e2141e57a4a2f290d922
Catalyst Students <accounts@catalystclass.com>
2012-12-02 16:23:29 -0800

    Fixed/finished functions in Koans.

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -104,9 +104,6 @@ describe("About Functions", function() {
       //An internal comment
       return a * b;
     };
-    expect(multiply.toString()).toBe("function (a, b) {
-      //An internal comment
-      return a * b;
-    }");
+    expect(multiply.toString()).toBe("function (a, b) {\n      //An internal comment\n      return a * b;\n    }");
   });    
 });
