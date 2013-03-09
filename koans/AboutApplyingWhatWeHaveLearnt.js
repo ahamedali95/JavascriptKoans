aebec2dea19310340aa71724de8458b8fad5599c
Hack Reactor Students <accounts@hackreactor.com>
2013-03-09 12:07:47 -0800

    Complete Javacript Koans exercise

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -41,7 +41,7 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(0);
   });
 
   /*********************************************************************************/
@@ -55,17 +55,28 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
     var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var result = _.range(1000).chain()
+                .reduce(function(memo,x) {return memo + x});
+                .value();
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(result);
+
   });
 
-  /*********************************************************************************/
+ var result = _([ [0, 1], 2 ]).chain()
+                     .flatten()
+                     .map(function(x) { return x+1 } )
+                     .reduce(function (sum, x) { return sum + x })
+                     .value();
+
+    expect(result).toEqual(6);
+      /*********************************************************************************/
    it("should count the ingredient occurrence (imperative)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
