fc9735496411707635e58e0593eefddcb8cc7fa7
Hack Reactor Students <accounts@hackreactor.com>
2013-03-09 11:59:54 -0800

    Everything except Put it All Together

--- koans/AboutMutability.js
+++ koans/AboutMutability.js
@@ -3,8 +3,8 @@ describe("About Mutability", function() {
   it("should expect object properties to be public and mutable", function () {
     var aPerson = {firstname: "John", lastname: "Smith" };
     aPerson.firstname = "Alan";
-    
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+
+    expect(aPerson.firstname).toBe("Alan");
   });
 
   it("should understand that constructed properties are public and mutable", function () {
@@ -15,8 +15,8 @@ describe("About Mutability", function() {
     }
     var aPerson = new Person ("John", "Smith");
     aPerson.firstname = "Alan";
-    
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+
+    expect(aPerson.firstname).toBe("Alan");
   });
 
   it("should expect prototype properties to be public and mutable", function () {
@@ -28,22 +28,22 @@ describe("About Mutability", function() {
     Person.prototype.getFullName = function () {
       return this.firstname + " " + this.lastname;
     };
-    
+
     var aPerson = new Person ("John", "Smith");
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
-    
+    expect(aPerson.getFullName()).toBe("John Smith");
+
     aPerson.getFullName = function () {
       return this.lastname + ", " + this.firstname;
     };
-    
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+
+    expect(aPerson.getFullName()).toBe("Smith, John");
   });
 
   it("should know that variables inside a constructor and constructor args are private", function () {
     function Person(firstname, lastname)
     {
       var fullName = firstname + " " + lastname;
-      
+
       this.getFirstName = function () { return firstname; };
       this.getLastName = function () { return lastname; };
       this.getFullName = function () { return fullName; };
@@ -53,16 +53,16 @@ describe("About Mutability", function() {
     aPerson.firstname = "Penny";
     aPerson.lastname = "Andrews";
     aPerson.fullName = "Penny Andrews";
-    
-    expect(aPerson.getFirstName()).toBe(FILL_ME_IN);
-    expect(aPerson.getLastName()).toBe(FILL_ME_IN);
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+
+    expect(aPerson.getFirstName()).toBe("John");
+    expect(aPerson.getLastName()).toBe("Smith");
+    expect(aPerson.getFullName()).toBe("John Smith");
 
     aPerson.getFullName = function () {
       return aPerson.lastname + ", " + aPerson.firstname;
     };
-    
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+
+    expect(aPerson.getFullName()).toBe("Andrews, Penny");
   });
 
 });
